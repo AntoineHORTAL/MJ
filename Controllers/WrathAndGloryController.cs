6c1c48b9dc8e04c91f80cf6cf88459e38daa06df
Malgus__ <antoinehortal201@gmail.com>
2024-12-03 14:26:54 +0100

    modifs page accueuil WAG

--- Controllers/WrathAndGloryController.cs
+++ Controllers/WrathAndGloryController.cs
@@ -8,11 +8,50 @@ namespace MJ.Controllers
 {
     public class WrathAndGloryController : Controller
     {
+        #region Système Gilead
         public ActionResult Avachrus()
         {
             return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
         }
-        
+        public ActionResult Nethreus()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult Ostia()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult Enoch()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult GileadPrimus()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult Charibdion()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult BourbierDuVide()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult PersephonesRegret()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult Vulkaris()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        public ActionResult Trollius()
+        {
+            return View("~/Views/JDR/WrathAndGlory/Systeme_Gilead/Avachrus.cshtml");
+        }
+        #endregion
+
+
         public ActionResult EmployeursListe()
         {
             return View("~/Views/JDR/WrathAndGlory/EmployeursList.cshtml");
